13b3a139ee3fe2729f73540a54b5dbfe123077fc
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-09-26 12:30:38 -0400

    Initial Commit. Database.yml file added to .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PropertyPro::Application.config.secret_key_base = 'bb90a3afb1df848bd39f4ae5321fb9502ddd408c30faea201e3c62303ef0977975113c3445bff8c8df7b9f7cf8eb8f2ab3cf6f24bdfc6500f2946ff886ae4144'
