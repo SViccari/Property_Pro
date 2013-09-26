f0a8c6a9cc47c34ad9f7a64e6ad974671ff0df52
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-09-26 16:13:22 -0400

    Created Owner model. Unit tests are green.

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Owner do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :buildings }
+
+  it { should have_valid(:first_name).when('Lloyd', 'Harry') }
+  it { should_not have_valid(:first_name).when('',nil) }
+
+  it { should have_valid(:last_name).when('Christmas', 'Dunne') }
+  it { should_not have_valid(:last_name).when('',nil) }
+
+  it { should have_valid(:email).when('Lloyd@gmail.com', '1Harrymarry@hotmail.com') }
+  it { should_not have_valid(:email).when('',nil) }
+
 end
